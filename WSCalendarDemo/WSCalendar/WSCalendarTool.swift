17ca3917513410c295ed9091eb26da6a333e8857
WS <a739002611@outlook.com>
2017-06-02 11:23:54 +0800

    2

--- WSCalendarDemo/WSCalendar/WSCalendarTool.swift
+++ WSCalendarDemo/WSCalendar/WSCalendarTool.swift
@@ -21,24 +21,20 @@ class WSCalendarTool {
 
 //MARK:- public property
     public let disableScrollingBeforeDate: Date = Date()
-    public let lastSelectableDate: Date = {
-        var dayComponents = DateComponents()
-        dayComponents.day = 10
-        let calendar = Calendar(identifier: .gregorian)
-        if let lastDate = calendar.date(byAdding: dayComponents, to: Date()) {
-            return lastDate
-        } else {
-            return Date()
-        }
-    }()
+    public let lastSelectableDate: Date = Date()
     
 //MARK:- private property
     private let calendar: Calendar = Calendar(identifier: .gregorian)
+    let dateFormatter = DateFormatter()
 
 //MARK:- public func
     public func getAllDate() -> [WSCalendarDate] {
         
         return getDaysInMonth(disableScrollingBeforeDate)
+    }
+    
+    init() {
+        lastSelectableDate = getDate(Date(), 10)
     }
 
 //MARK:- private func
@@ -54,7 +50,7 @@ class WSCalendarTool {
     
     private func getDaysInMonth(_ date: Date) -> [WSCalendarDate] {
         
-        let dateFormatter = DateFormatter()
+        
         dateFormatter.dateFormat = "yyyyMM"
         let nowDateBaseStr = dateFormatter.string(from: date)
         dateFormatter.dateFormat = "yyyyMMdd"
@@ -64,12 +60,16 @@ class WSCalendarTool {
         
         let maxSelectableDays = getDayNum(disableScrollingBeforeDate, lastSelectableDate)
         let currentMonthDays = getNumberOfDaysInMonth(date)
+        //当月的第一天和最后一天
+        var firstDate: WSCalendarDate! ,lastDate: WSCalendarDate!
+        
         
         for i in 1...currentMonthDays {
             let str = nowDateBaseStr.appending(String(format: "%02d", i))
             let tmpDate = dateFormatter.date(from: str)!
             var calendarDate: WSCalendarDate = WSCalendarDate(date: tmpDate, dateString: str, isSelectable: false, isSelect: false)
             
+            //特殊的日子判断
             if (str == nowDateStr) {
                 calendarDate.isSelect = true
                 calendarDate.isSelectable = true
@@ -77,19 +77,60 @@ class WSCalendarTool {
                 calendarDate.isSelectable = true
             }
             
+            //超出的日期
             if i > maxSelectableDays {
                 calendarDate.isSelectable = false
+            }
+            
+            //收尾的天数
+            if i == 1 {
+                firstDate = calendarDate
+            }else if i == currentMonthDays {
+                lastDate = calendarDate
             }
             
             tmpArr.append(calendarDate)
         }
         
-        return tmpArr
+        return insertDaysAtBothEnds(firstDate, lastDate, tmpArr)
     }
     
     private func getDayNum(_ date: Date,_ betweenDate: Date) -> Int {
         let result = calendar.dateComponents([.day], from: date, to: betweenDate)
 
         return result.day ?? 0
+    }
+    
+    private func insertDaysAtBothEnds(_ fromDate:WSCalendarDate,_ toDate:WSCalendarDate, _ dataArr:[WSCalendarDate]) -> [WSCalendarDate]{
+        
+        let fromWeekIndex = calendar.dateComponents([.weekday], from: fromDate.date).weekday
+        let toWeekIndex = calendar.dateComponents([.weekday], from: toDate.date).weekday
+        
+        
+        if let fromIndex = fromWeekIndex {
+            for i in 1..<fromIndex {
+                let date = getDate(fromDate.date, i - fromIndex)
+                WSCalendarDate(date:date , dateString: getDateStr(date), isSelectable: false, isSelect: false)
+            }
+        }
+        
+        
+        return dataArr
+    }
+    
+    private func getDate(_ date: Date,_ offset:Int) -> Date{
+        var dayComponents = DateComponents()
+        dayComponents.day = offset
+        let calendar = Calendar(identifier: .gregorian)
+        if let lastDate = calendar.date(byAdding: dayComponents, to: date) {
+            return lastDate
+        } else {
+            return Date()
+        }
+    }
+    
+    private func getDateStr(_ date: Date) -> String {
+        dateFormatter.dateFormat = "yyyyMMdd"
+        return dateFormatter.string(from: date)
     }
 }
